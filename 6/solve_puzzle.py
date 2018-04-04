d067bc6daaf5178f3265e009e886c580aa6ceb1d
AZ <alvinzhu33@yahoo.com>
2018-04-04 12:07:51 -0400

    Stuff

--- 6/solve_puzzle.py
+++ 6/solve_puzzle.py
@@ -1,4 +1,36 @@
+def conversion(config):
+    list = [];
+    zero = (0, 0);
+    for y in config:
+        row = [];
+        for x in y:
+            row += [x];
+            if x == 0:
+                zero = (y, x);
+        list += [row];
+    return list, zero;
+def computeAnswer(config):
+    track = len(config)*len(config[1])-1;
+    for y in range(len(config)):
+        for x in range(len(config[y])):
+            config[y][x] = track;
+            track -= 1;
+    return config;
+
 def solve_puzzle(config):
     '''Returns move sequence to solve configuration, or None'''
     moves = []
+    zero = ();
+    if isinstance(config, tuple):
+        config, zero = conversion(config);
+    answer = computeAnswer(config);
+    if config != answer:
+        moves += cont_solve_puzzle(config, zero, answer);
     return moves
+
+def cont_solve_puzzle(config, zero, answer):
+    if config != answer:
+        #check if anythinga above 0
+        #check if anythinga right of 0
+
+solve_puzzle(((3, 2), (1, 0)));
