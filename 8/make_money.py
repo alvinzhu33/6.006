757cd85136293c54df014e09e836c0b22863655d
AZ <alvinzhu33@yahoo.com>
2018-04-20 19:39:13 -0400

    Yes No Maybe

--- 8/make_money.py
+++ 8/make_money.py
@@ -1,3 +1,5 @@
+from math import log;
+
 def make_money(exchange_rate):
     '''
     Find list of currencies to exchange and make money.
@@ -6,4 +8,54 @@ def make_money(exchange_rate):
             starting and ending with currency 0, that is profitable,
             or None if no such sequence exists.
     '''
-    return None
+    d = [[float("inf"), 0]]*len(exchange_rate);
+    d[0][0] = 0.0;
+    found = 0;
+    change = False;
+
+    for relax in range(len(d)):
+        for i in range(len(d)):
+            for j in range(len(d)):
+                # print(i, j, d[i], -log(exchange_rate[i][j]))
+                if d[i][0] + -log(exchange_rate[i][j]) < d[j][0]:
+                    change = True;
+                    d[j] = [d[i][0] + -log(exchange_rate[i][j]), i];
+        if not change:
+            return None;
+    for i in range(len(d)):
+        for j in range(len(d)):
+            if d[j][0] > d[i][0] + -log(exchange_rate[i][j]):
+                found = j;
+
+    cycle = [found];
+    foundSet = set();
+    cycleSet = set();
+    found = d[found][1];
+    while found not in cycleSet:
+        if found in foundSet:
+            cycleSet.add(found);
+            cycle += [found];
+        else: foundSet.add(found);
+        found = d[found][1];
+    cycle = cycle[len(cycle)-1:0:-1]
+    print(cycle);
+
+    conversion = 1;
+    i = 0;
+    counter = 0;
+    j = cycle[counter];
+    path = [];
+    while True:
+        path += [i];
+        conversion = conversion * exchange_rate[i][j];
+        if conversion * exchange_rate[j][0] > 1:
+            path += [j, 0];
+            conversion = conversion * exchange_rate[j][0];
+            break;
+        i = j;
+        counter += 1;
+        if counter >= len(cycle):
+            counter = counter % len(cycle);
+        j = cycle[counter];
+    print(path);
+    return path
